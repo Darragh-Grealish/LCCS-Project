caf04f04128c065a7b99c7b85d0c5ea47436aee1
Darragh-Grealish <97552437+Darragh-Grealish@users.noreply.github.com>
2022-03-22 20:06:12 +0000

    Add files via upload

---
+++ Artefact/Website Code/LCCS JS Functions.js
@@ -0,0 +1,9 @@
+
+
+//-------------------------------Text Size ------------------------------------------------------------------
+function regular(){ 
+        document.getElementById('nav').style.fontSize='16px';
+    }
+    function large(){
+        document.getElementById('nav').style.fontSize='19px';
+    }
